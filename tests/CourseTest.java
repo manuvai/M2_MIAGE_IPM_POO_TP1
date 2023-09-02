3e738103e7bfac344a3d23283d9e0c43be3a0973
manuvai <rm90266@gmail.com>
2023-09-02 22:34:15 +0200

    Ajout de sources

---
+++ tests/CourseTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import res.Athlete;
+import res.Couloir;
+import res.Course;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.NoSuchElementException;
+
+public class CourseTest {
+
+    @Test
+    public void getConcurrent_couloirListNull_Ko() {
+        // GIVEN
+        Course c = new Course();
+
+        // WHEN
+        Athlete a = c.getVainqueur();
+
+        // THEN
+        Assert.assertNull(a);
+    }
+    @Test
+    public void getConcurrent_couloirListEmpty_Ko() {
+        // GIVEN
+        Course c = new Course();
+        c.setCouloirList(new ArrayList<>());
+
+        // WHEN
+        Athlete a = c.getVainqueur();
+
+        // THEN
+        Assert.assertNull(a);
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void getConcurrent_couloirListWithNoAthlete_Ko() {
+        // GIVEN
+        Course c = new Course();
+        c.setCouloirList(Collections.singletonList(new Couloir()));
+
+        // WHEN
+        c.getVainqueur();
+    }
+}
